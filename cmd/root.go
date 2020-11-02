a1f555acbb760516d9559bb878dc8bbe8ee186a4
xuan zitao <xuan_zitao@126.com>
2020-11-02 22:54:11 +0800

    version 2.0.4 change help text

--- cmd/root.go
+++ cmd/root.go
@@ -29,20 +29,10 @@ var (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "x-to-mysql",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Short: "x-to-mysql is a useful tool for you to export data from csv/sqlite to mysql",
+	Long: `x-to-mysql is a useful tool for you to export data from csv/sqlite to mysql`,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
